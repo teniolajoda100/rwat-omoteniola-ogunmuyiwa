2ee0294d975c5f0ba1bf9e7fb09f30eb6180fa0f
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2025-12-13 20:04:26 +0000

    Fixed deployment to seperately repository. I initially misunderstood assignment requirements. I was deploying to the same repo from the github classroom rather than a seperate repository. I created a seperate one for deployment. i updated my workflow to use my deployment repository. My source code stays in developemnt repo for submisssion as ZIP file

--- CA2/vite.config.js
+++ CA2/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/rwat-omoteniola-ogunmuyiwa/',
+    base: '/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
