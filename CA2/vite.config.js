0220d1b53c4c827329fd5af068a736e5c6d904a8
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2025-12-13 20:32:16 +0000

    Fixed vite base path, deployment fixes.

--- CA2/vite.config.js
+++ CA2/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/',
+    base: '/memory-game-ca2/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
